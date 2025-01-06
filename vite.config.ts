d35b5661182e797af97b67243e08ce9f7b49e72c
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-01-05 21:38:41 -0300

    Update config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
+    },
+    define: {
+        SUPABASE_URL: process.env.SUPABASE_URL,
+        SUPABASE_KEY: process.env.SUPABASE_KEY
     }
-  },
-  define: {
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_KEY: process.env.SUPABASE_KEY,
-  }
 });
